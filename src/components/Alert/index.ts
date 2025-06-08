475331a3aaf0c761447eef3644809dbe1e3e1354
mrprotocoll <lekanvgbg@gmail.com>
2025-06-08 22:30:38 +0100

    fix alert export

--- src/components/Alert/index.ts
+++ src/components/Alert/index.ts
@@ -1,5 +1,5 @@
 export { AlertProvider, useAlert, AlertContext, alert } from "./AlertContext";
-export { default } from './ConfirmationAlert';
+export { default as ConfirmationAlert} from './ConfirmationAlert';
 export type { AlertType, AlertProps } from "./Alert";
 export type {AlertContextProps, AlertItem} from "./AlertContext";
 export type { ConfirmationPopupProps, ConfirmationType } from './ConfirmationAlert'
