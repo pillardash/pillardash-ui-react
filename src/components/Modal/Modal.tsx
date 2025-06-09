e5c131c7ea5a81e7dddeaebf796243890aa1c368
mrprotocoll <lekanvgbg@gmail.com>
2025-06-09 20:41:33 +0100

    fix modal content scroll

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -177,7 +177,7 @@ const Modal: React.FC<ModalProps> = ({
                     )}
 
                     {/* Content */}
-                    <div className={`flex-1 min-h-0 ${maxHeight ? "overflow-y-auto" : ""} ${contentClassName}`}>
+                    <div className={`flex-1 min-h-0 overflow-y-auto ${maxHeight ? "overflow-y-auto" : ""} ${contentClassName}`}>
                         {maxHeight && (position === "left" || position === "right") ? (
                             <div className="p-6 min-h-0 flex-1">
                                 {children}
