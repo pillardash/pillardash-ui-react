6dec031d47b682aa62e9377f00a91231edf9893b
mrprotocoll <lekanvgbg@gmail.com>
2025-06-10 22:29:14 +0100

    update Card

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-const Card = ({ children, className }: { children: ReactNode, className: string }) => {
+const Card = ({ children, className = "" }: { children: ReactNode, className?: string }) => {
     return (
         <div className={`w-full rounded-[20px] border border-[#EEEEF0] bg-white p-4 ${className}`}>{children}</div>
     );
